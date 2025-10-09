a16ce4dea9b82dba71e028d00f2408e883d6da99
Rafael Crempe <rafaelcrempe@gmail.com>
2025-10-08 21:33:14 -0300

    Day 1

---
+++ Day_1/datatypes.js
@@ -0,0 +1,8 @@
+/* 5. Create datatypes.js file and use the JavaScript typeof operator to check different data types. 
+ Check the data type of each variable */
+
+console.log(typeof 'Rafael') // string
+console.log(typeof 32) // number
+console.log(typeof true) // boolean
+console.log(typeof null) // object type
+console.log(typeof undefined) // undefined
---
+++ Day_1/variable.js
@@ -0,0 +1,8 @@
+// 4. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+let name = 'Rafael' //string
+let isMarried = false //boolean
+let age   //undefined
+let empty = null //null
+
+
