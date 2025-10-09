805edc7b54c05b22b37b825f8ec9473e8798a28b
Rafael Crempe <rafaelcrempe@gmail.com>
2025-10-09 17:01:23 -0300

    exercises level 1 - part 1

---
+++ day_2/exercises_l1.js
@@ -0,0 +1,72 @@
+
+// 1. Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript'.
+
+let challenge = '30 Days of JavaScript'
+
+
+// 2. Print the string on the browser console using console.log()
+
+console.log(challenge)
+
+// 3. Print the length of the string on the browser console using console.log()
+
+console.log(challenge.length)
+
+// 4. Change all the string characters to capital letters using toUpperCase() method
+
+console.log(challenge.toUpperCase())
+
+// 5. Change all the string characters to lowercase letters using toLowerCase() method
+
+console.log(challenge.toLowerCase())
+
+// 6. Cut (slice) out the first word of the string using substr() or substring() method
+
+console.log(challenge.substring(0,3))
+
+// 7. Slice out the phrase Days Of JavaScript from 30 Days Of JavaScript.
+
+console.log(challenge.substring(-1, 3))
+
+// 8. Check if the string contains a word Script using includes() method
+
+console.log(challenge.includes('Script'))
+
+// 9. Split the string into an array using split() method
+
+console.log(challenge.split())
+
+// 10. Split the string 30 Days Of JavaScript at the space using split() method
+
+console.log(challenge.split(' '))
+
+// 11. 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
+
+let string = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+
+console.log(string.split(','))
+
+// 12. Change 30 Days Of JavaScript to 30 Days Of Python using replace() method.
+
+console.log(challenge.replace('JavaScript', 'Python'))
+
+
+/*
+
+What is character at index 15 in '30 Days Of JavaScript' string? Use charAt() method.
+What is the character code of J in '30 Days Of JavaScript' string using charCodeAt()
+Use indexOf to determine the position of the first occurrence of a in 30 Days Of JavaScript
+Use lastIndexOf to determine the position of the last occurrence of a in 30 Days Of JavaScript.
+Use indexOf to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+Use lastIndexOf to find the position of the last occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+Use search to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+Use trim() to remove any trailing whitespace at the beginning and the end of a string.E.g ' 30 Days Of JavaScript '.
+Use startsWith() method with the string 30 Days Of JavaScript and make the result true
+Use endsWith() method with the string 30 Days Of JavaScript and make the result true
+Use match() method to find all the a’s in 30 Days Of JavaScript
+Use concat() and merge '30 Days of' and 'JavaScript' to a single string, '30 Days Of JavaScript'
+Use repeat() method to print 30 Days Of JavaScript 2 times */
+
+
+
+
