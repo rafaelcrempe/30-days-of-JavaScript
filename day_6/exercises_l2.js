b4d03916b9ea83b960896612daa25ca56296b375
Rafael Crempe <rafaelcrempe@gmail.com>
2025-12-23 22:10:51 -0300

    day 6 lvl 2

--- day_6/exercises_l2.js
+++ day_6/exercises_l2.js
@@ -12,6 +12,17 @@ const countries = [
   'Kenya'
 ]
 
+const webTechs = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Redux',
+  'Node',
+  'MongoDB'
+]
+
+const mernStack = ['MongoDB', 'Express', 'React', 'Node']
 
 // 1. Develop a small script which generate any number of characters random id
 
@@ -59,3 +70,106 @@ upperCaseArray.push(countries[i].toUpperCase())
 }
 
 console.log(upperCaseArray)
+
+// 5. Using the above countries array, create an array for countries length.
+
+let arrayWordLength = []
+
+for (i = 0; i < countries.length; i++) {
+  arrayWordLength.push(countries[i].length)
+}
+
+console.log(arrayWordLength)
+
+// 6. Use the countries array to create the following array of arrays:
+
+let ArrayOfArrays = []
+
+for (i = 0; i < countries.length; i++) {
+  let country = countries[i].slice(0, 3).toUpperCase()
+  let number = countries[i].length
+
+  ArrayOfArrays.push([countries[i], country, number])
+}
+
+console.log(ArrayOfArrays)
+
+/* 7. In above countries array, check if there is a country or countries containing the word 'land'. 
+If there are countries containing 'land', print it as array. 
+If there is no country containing the word 'land', print 'All these countries are without land'. */
+
+let countriesWithLand = []
+
+for(i=0; i < countries.length; i++) {
+  
+  if (countries[i].includes('land')) {
+    countriesWithLand.push(countries[i])
+  }
+}
+
+if (countriesWithLand.length > 0) {
+  console.log(countriesWithLand);
+} else {
+  console.log('All these countries are without land');
+}
+
+/* 8. In above countries array, check if there is a country or countries end with a substring 'ia'. 
+If there are countries end with, print it as array. 
+If there is no country containing the word 'ai', print 'These are countries ends without ia'.*/
+
+let countriesEndingIa = []
+
+for(i=0; i < countries.length; i++) {
+  
+  if (countries[i].endsWith('ia')) {
+    countriesEndingIa.push(countries[i])
+  }
+}
+
+if (countriesEndingIa.length > 0) {
+  console.log(countriesEndingIa);
+} else {
+  console.log("These are countries ends without 'ia'");
+}
+
+// 9. Using the above countries array, find the country containing the biggest number of characters.
+
+let longestCountry = '';
+let length = 0;
+
+for (let i = 0; i < countries.length; i++) {
+  if (countries[i].length > length) {
+    length = countries[i].length;
+    longestCountry = countries[i];
+  }
+}
+
+console.log(`The country with the biggest number of characters is: ${longestCountry}`);
+
+// 10. Using the above countries array, find the country containing only 5 characters.
+
+let countriesWith5 = []
+
+for (let i = 0; i < countries.length; i++) {
+  if (countries[i].length === 5) {
+    countriesWith5.push(countries[i])
+  }
+}
+
+console.log(countriesWith5)
+
+// 11. Find the longest word in the webTechs array
+
+let longestWord = '';
+let wordSize = 0;
+
+for (let i = 0; i < webTechs.length; i++) {
+  if (webTechs[i].length > length) {
+    longestWord = webTechs[i].length;
+    longestWord = webTechs[i];
+  }
+}
+
+console.log(`The longest word in the webTechs array is '${longestWord}'`);
+
+// 12. 
